927d8f1496969c2341153dba2318d10d56bd5947
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-02-04 14:16:25 +0200

    Add domain models for movies and genres

---
+++ WebApplication2/models/Gener.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace WebApplication2.models
+{
+    public class Gener
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public byte Id { get; set; }
+
+        [MaxLength ( 100)]
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication2/models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Principal;
+
+namespace WebApplication2.models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+
+        [MaxLength(2500)]
+        public string storeline { get; set; }
+
+        public int year { get; set; }   
+
+        public byte[] Poster {  get; set; }
+        public double Rate { get; set; }
+
+        public byte generId {  get; set; }
+        
+        public Gener  gener { get; set; }   
+
+    }
+}
