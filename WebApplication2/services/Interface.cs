c99052dc9ad67dece9bbda936ef203783bc2be17
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-02-04 14:17:24 +0200

    Add service interfaces for business logic

---
+++ WebApplication2/services/Interface.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication2.models;
+
+namespace WebApplication2.services
+{
+    public interface IGenerservices
+    {
+        Task<IEnumerable<Gener>> getall();
+        Task<Gener> update(Gener gener);    
+        Task<Gener> delete(Gener gener);
+        Task<Gener> add (Gener gener);
+        Task<Gener> getbyid(byte id);
+        Task<bool> isvalid(byte id);
+    }
+}
