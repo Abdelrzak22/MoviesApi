8d37688c54b4a8bee4f42712aede0e6b33f76f39
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-02-04 14:17:09 +0200

    Create DTOs for movie and genre operations

---
+++ WebApplication2/Dto/CreateGenredto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Dto
+{
+    public class CreateGenredto
+    {
+
+        [MaxLength(100)]    
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication2/Dto/Moviedt.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication2.Dto
+{
+    public class Moviedt
+    {
+
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+
+        [MaxLength(2500)]
+        public string storeline { get; set; }
+
+        public int year { get; set; }
+
+        public IFormFile Poster { get; set; }
+        public double Rate { get; set; }
+
+        public byte generId { get; set; }
+    }
+}
