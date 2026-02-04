eefece91c8711f3e3d93c4958dee1c95a04f6136
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-02-04 14:16:40 +0200

    Configure Entity Framework DbContext

---
+++ WebApplication2/dbcontext/Applicationdbcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.models;
+
+namespace WebApplication2.dbcontext
+{
+    public class Applicationdbcontext : DbContext
+    {
+        public Applicationdbcontext(DbContextOptions<Applicationdbcontext> options):base(options) { }
+
+        public DbSet<Gener> Geners { get; set; }    
+        public DbSet<Movie> Movies { get; set; }    
+    }
+}
